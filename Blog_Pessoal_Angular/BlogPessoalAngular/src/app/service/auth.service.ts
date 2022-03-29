0646a0d94d36fb91f83776141d6d466ee5794b2f
agathacamargo23 <agatha_rafel@hotmail.com>
2022-03-29 18:45:08 -0300

    consumo-parte1

---
+++ Blog_Pessoal_Angular/BlogPessoalAngular/src/app/service/auth.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Usuario } from '../model/Usuario';
+import { UsuarioLogin } from '../model/Usuario.Login';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(
+    private html: HttpClient) {}//libera os metodos http
+
+/*
+certificar os endpoints
+*/
+
+entrar(usuarioLogin: UsuarioLogin): Observable<UsuarioLogin>{
+return this.html.post<UsuarioLogin>('http://localhost:8080/usuarios/logar', usuarioLogin)
+}
+Cadastrar(usuario: Usuario): Observable<Usuario> {// da referencia a model do usuario
+
+return this.html.post<Usuario>('http://localhost:8080/usuarios/cadastrar', usuario)  
+  }// sinaliza a model usuario model e o usuario controller 
+}
